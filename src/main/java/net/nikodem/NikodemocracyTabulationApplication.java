d8d78600194224bc7142ca04aa80eaaba01bf87e
Peter Nikodem <peternikodemjr@gmail.com>
2016-05-13 22:40:32 +0200

    Initial commit

---
+++ src/main/java/net/nikodem/NikodemocracyTabulationApplication.java
@@ -0,0 +1,12 @@
+package net.nikodem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NikodemocracyTabulationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NikodemocracyTabulationApplication.class, args);
+	}
+}
---
+++ src/test/java/net/nikodem/NikodemocracyTabulationApplicationTests.java
@@ -0,0 +1,18 @@
+package net.nikodem;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = NikodemocracyTabulationApplication.class)
+@WebAppConfiguration
+public class NikodemocracyTabulationApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
